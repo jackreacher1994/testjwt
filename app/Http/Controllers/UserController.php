658ffda599418b4f1c296cc216bc74025d26394e
jackreacher1994 <jackreacher1994@gmail.com>
2017-10-21 20:14:49 +0700

    Initiation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use JWTAuthException;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+        $token = null;
+        try {
+           if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['invalid_email_or_password'], 422);
+           }
+        } catch (JWTAuthException $e) {
+            return response()->json(['failed_to_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
+
+    public function getAuthUser(Request $request){
+        $user = JWTAuth::toUser($request->token);
+        return response()->json(['result' => $user]);
+    }
+}
